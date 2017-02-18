ba0f87b61baf19ca8cec3dd67be5170b5fcb3b98
Valentin Malykh <madrugado@yandex.ru>
2017-02-18 15:32:49 +0300

    ENH: clean up after work

--- download_coco_inception_features.sh
+++ download_coco_inception_features.sh
@@ -3,3 +3,4 @@
 mkdir -p ./cache/mscoco_image_features
 wget -O ./cache/mscoco_image_features/inception_v3.zip http://www.cs.jhu.edu/~jhmao/open_source_data/tf_mrnn/inception_v3.zip
 unzip ./cache/mscoco_image_features/inception_v3.zip -d ./cache/mscoco_image_features/
+rm ./cache/mscoco_image_features/inception_v3.zip
--- download_coco_vgg_features.sh
+++ download_coco_vgg_features.sh
@@ -3,4 +3,4 @@
 mkdir -p ./cache/mscoco_image_features
 wget -O ./cache/mscoco_image_features/vgg_l15.zip http://www.cs.jhu.edu/~jhmao/open_source_data/tf_mrnn/vgg_l15.zip
 unzip ./cache/mscoco_image_features/vgg_l15.zip -d ./cache/mscoco_image_features/
-
+rm ./cache/mscoco_image_features/vgg_l15.zip
